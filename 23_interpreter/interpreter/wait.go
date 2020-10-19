ce3aa2c97aed7aed9d8cb93d6e3e989053fa1133
yokomichi <yokomichi.masanobu@willgate.co.jp>
2020-10-19 09:23:17 +0900

    feat : interpreter

---
+++ 23_interpreter/interpreter/wait.go
@@ -0,0 +1,17 @@
+package interpreter
+
+type Wait struct {
+	minutes int
+	operand *Operand
+}
+
+func NewWait(minutes int, operand *Operand) *Wait{
+	return &Wait{
+		minutes: minutes,
+		operand: operand,
+	}
+}
+
+func (w *Wait) Execute() Operand{
+	return NewIngrediend(w.operand.GetOperandString() + "を" + string(w.minutes) + "置いたもの")
+}
